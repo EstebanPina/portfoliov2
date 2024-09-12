3bb8761c141af59cd29e33d9a9e081691fc76aec
Esteban Andrés Piña Cabañas <39716335+EstebanPina@users.noreply.github.com>
2024-09-12 00:04:07 -0600

    Work Experience

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,18 +15,18 @@ export default function Home() {
   return (
     <main className="font-roboto">
     <HeaderMe></HeaderMe>
-    <div className='flex flex-col gap-6 p-16 justify-start w-full min-h-screen bg-slate-950'>
+    <div className='flex flex-col gap-8 p-16 justify-start w-full min-h-screen bg-slate-950'>
       <h2 className='text-7xl font-bebas text-white'>Work Experience</h2>
       {work_experience.map((experience,index)=>{
         return(
-          <div key={index} className='font-roboto bg-white/10 shadow-xl shadow-indigo-200/20 p-4 rounded-md text-white'>
-            <h3 className='text-5xl font-bebas'>{experience.company}</h3>
-            <p className='text-3xl font-bebas'>{experience.position}</p>
-            <p className='text-xl font-bebas'>{experience.start} - {experience.end}</p>
+          <div key={index} className='font-roboto flex flex-col bg-white/10 shadow-xl shadow-indigo-400/20 px-8 py-4 hover:scale-105 duration-500 rounded-tr-3xl rounded-bl-3xl text-white'>
+            <h3 className='text-3xl font-bebas'>{experience.company}</h3>
+            <p className='text-xl font-bebas'>{experience.position}</p>
+            <p className='text-md font-bebas'>{experience.start} - {experience.end}</p>
             <div className='flex gap-2'>
             {experience.tech_stack.map((tech, index)=>{
               return(
-                <span key={index} className='text-xl font-bebas'>{tech_dict[tech]}</span>
+                <span key={index} className='text-xl my-2 font-bebas drop-shadow-2xl'>{tech_dict[tech]}</span>
               )
             })}
             </div>
